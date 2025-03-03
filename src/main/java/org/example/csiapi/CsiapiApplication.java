714a82d7c941b9ff18840c4d199b34c4d3bb07ce
xiaohan <bornwarm@foxmail.com>
2025-03-03 10:58:13 +0800

    Init project.

---
+++ src/main/java/org/example/csiapi/CsiapiApplication.java
@@ -0,0 +1,13 @@
+package org.example.csiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsiapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsiapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/csiapi/CsiapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.csiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsiapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
